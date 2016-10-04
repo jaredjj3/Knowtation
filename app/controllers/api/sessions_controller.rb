81169883dca6da767f604fb4a874f97509538192
App Academy Student <appacademy@Apps-Mac-mini-70.local>
2016-10-04 10:53:04 -0400

    start sessions controller

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,24 @@
+class Api::SessionsController < ApplicationController
+  def create
+    username = params[:user][:username]
+    password = params[:user][:password]
+    @user = User.find_by_credentials
+
+    if @user
+      login(@user)
+      render :show
+    else
+      render json: ["Invalid username/password combination"]
+    end
+
+  end
+
+  def destroy
+    if current_user
+      logout
+      render json: {}
+    else
+      render json: {}
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
     resources :users, only: [ :create, :show, :update ]
+    resource :session, only: [ :create, :destroy ]
   end
 end
