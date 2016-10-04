84b9620d4dae1c788cacccbeac1f58e19c381601
App Academy Student <appacademy@Apps-Mac-mini-70.local>
2016-10-04 11:51:03 -0400

    update README to reflect project

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -1,8 +1,36 @@
 import React from 'react';
 
 class SessionForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleOnChange = this.handleOnChange.bind(this);
+  }
 
-  render() {}
+  handleOnChange(e) {
+    
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const user = this.state;
+    this.props.processForm({ user })
+  }
+
+
+  render() {
+    return (
+      <form>
+        <label>Username <input type="text"></label>
+        <label>Password <input type="password"></label>
+        <input type="button" value="Submit">
+      </form>
+    );
+  }
 
 }
 
