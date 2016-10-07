99e6f86452f60fde2317679a8efc50df3361234f
Jared Johnson <Jared@Jareds-MacBook-Pro.local>
2016-10-07 13:39:09 -0400

    continue making biography editable on frontend

--- frontend/components/profile/biography_text.jsx
+++ frontend/components/profile/biography_text.jsx
@@ -3,92 +3,109 @@ import React from 'react';
 class BiographyText extends React.Component {
   constructor(props) {
     super(props);
-    const { pageUser } = this.props;
+    const { currentUser, pageUser } = this.props;
     this.state = {
+      editing: false,
       country: pageUser.country,
       bio: pageUser.bio
     };
-    this.setupInputFields();
-    this.handleClick = this.handleClick.bind(this);
-    this.setupInputFields = this.setupInputFields.bind(this);
+    this.handleEditClick = this.handleEditClick.bind(this);
+    this._pageIsCurrentUser = this._pageIsCurrentUser.bind(this);
   }
 
-  handleClick(property) {
-    return e => {
-      this.setState({
-        [property]: 'asdfsdf'
-      });
-    };
+  handleEditClick(e) {
+    this.setState({
+      editing: true
+    });
   }
-
-  _nullHandleClick(property) {
-    return e => {};
-  }
-
-  // Displays the information as input fields if the currentUser
-  // is the pageUser
-  setupInputFields() {
-    const { currentUser, pageUser } = this.props;
-    if (currentUser.id === pageUser.id) {
-      const { username, country, userType, bio } = currentUser;
-      this.countryDisplay = this._countryDisplay(country);
-      this.userTypeDisplay = this._userTypeDisplay(userType);
-      this.bioDisplay = this._bioDisplay(bio);
-    } else {
-      const { username, country, userType, bio } = pageUser;
-      this.countryDisplay = <h2 className="profile-country">{ country }</h2>;
-      this.userTypeDisplay = <h3 className="profile-user-type">{ userType }</h3>;
-      this.bioDisplay = <p className="profile-bio">{ bio }</p>;
-    }
-  }
-
-  // Methods used for currentUser properties, not pageUser
-  // -----------------------------------------------------
-  _usernameDisplay(username) {
-
-  }
-
-  _countryDisplay(country) {
-    if (country === null) {
-      return <h2>Where are you from?</h2>;
-    } else {
-      return <h2>{ country }</h2>;
-    }
-  }
-
-  _userTypeDisplay(userType) {
-    if (userType === 'student') {
-      return <h3>Wanna be a teacher?</h3>;
-    } else {
-      return <h3>Teacher</h3>;
-    }
-  }
-
-  _bioDisplay(bio) {
-    if (bio === 'null') {
-      return <textarea placeholder="tell us about yourself"></textarea>;
-    } else {
-      return <p className="profile-bio">{ bio }</p>;
-    }
-  }
-
-  // -----------------------------------------------------
 
   render() {
     const { currentUser, pageUser } = this.props;
     const { username, country, userType, bio } = pageUser;
+    const { pageIsCurrentUser } = this.state;
 
     return(
       <div className="biography-text">
-        <h1 className="profile-username">{ username }</h1>
-        { this.countryDisplay }
-        { this.userTypeDisplay }
-        { this.bioDisplay }
+        { this._iconDisplay() }
+        { this._usernameDisplay(username) }
+        { this._userTypeDisplay(userType) }
+        { this._countryDisplay(country) }
+        { this._bioDisplay(bio) }
       </div>
     );
   }
 
+  _pageIsCurrentUser() {
+    const { currentUser, pageUser } = this.props;
+    return currentUser.id === pageUser.id;
+  }
 
-};
+  _usernameDisplay(username) {
+    return <h1 className="profile-username">{ username }</h1>;
+  }
+
+  _iconDisplay() {
+    const { editing } = this.state;
+    if (this._pageIsCurrentUser()) {
+      if (editing) {
+        return (
+          <ul className='profile-edit-buttons-container'>
+            <li>
+              <button
+                onClick={ this.handleEditClick }
+                className='main-button'
+              >
+                  Save
+              </button>
+            </li>
+            <li>
+              <button
+                onClick={ this.handleEditClick }
+                className='main-button'
+              >
+                  Discard
+              </button>
+            </li>
+          </ul>
+        );
+      } else {
+        return (
+          <ul className='profile-edit-buttons-container'>
+            <li>
+              <button
+                onClick={ this.handleEditClick }
+                className='main-button'
+              >
+                  Edit
+              </button>
+            </li>
+          </ul>
+        );
+      }
+    } else {
+      return <div></div>;
+    }
+  }
+
+  _countryDisplay(country) {
+    if (country === null && this._pageIsCurrentUser()) {
+      return <h2 className="null-profile-country">country</h2>;
+    } else {
+      return <h2 className="profile-country">{ country }</h2>;
+    }
+  }
+
+  _userTypeDisplay(userType) {
+    return <h3 className="profile-user-type">{ userType }</h3>;
+  }
+
+  _bioDisplay(bio) {
+    if (bio === null && this._pageIsCurrentUser()) {
+      return <p className="null-profile-bio">tell us about yourself</p>;
+    } else {
+      return <p className="profile-bio">{ bio }</p>;
+    }
+  }
+}
 
 export default BiographyText;
