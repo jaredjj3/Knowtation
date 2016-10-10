2eeb451f40269ed01239330b28850935b202eaa4
Jared Johnson <Jared@Jareds-MacBook-Pro.local>
2016-10-10 19:36:15 -0400

    merge to resume working on profile

--- frontend/components/knowtation/knowtation.jsx
+++ frontend/components/knowtation/knowtation.jsx
@@ -9,12 +9,13 @@ class Knowtation extends React.Component {
   render() {
     const { knowtation } = this.props;
     const { videoUrl } = knowtation;
+    const modifiedVideoUrl = `${videoUrl.replace("watch?v=", "v/")}?autoplay=0&showinfo=0&controls=0`;
 
     return(
       <div className='knowtation-container group'>
         <iframe
           className='knowtation-video'
-          src={ videoUrl.replace }
+          src={ modifiedVideoUrl }
         />
       <div className='knowtation-settings'>I AM SETTINGS</div>
         <canvas className='knowtation-canvas' />
