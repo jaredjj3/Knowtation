1c8201f6501e0599090c26f9947f1c027f6ba4ab
Jared Johnson <Jared@Jareds-MacBook-Pro.local>
2016-10-11 23:33:21 -0400

    finish work for today

--- frontend/components/knowtation/notation_view.jsx
+++ frontend/components/knowtation/notation_view.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import Canvas from 'react-canvas-component';
 
-const NotationView = ({ knowtation }) => {
+class NotationView extends React.Component {
 
-  return (
-    <canvas id='canvas' className='notation-view' />
-  );
-};
+  componentWillReceiveProps(newProps) {
+    const { knowtation } = newProps;
+    const image = new Image();
+    image.src = knowtation.notationImageUrl;
+    console.log(knowtation.notationImageUrl);
+    const canvas = document.getElementById('canvas');
+    const context = canvas.getContext('2d');
+    image.onload = () => {
+      context.drawImage(image,
+        0, 0, image.width, image.height
+      );
+    };
+  }
+
+  render() {
+    return (
+      <div className='notation-view-container'>
+        <canvas id='canvas' className='notation-view' />
+      </div>
+    );
+  }
+}
 
 export default NotationView;
