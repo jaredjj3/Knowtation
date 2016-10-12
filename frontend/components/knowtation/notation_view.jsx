3fd4afc91253899fff21192baca746c3e6eba7e2
Jared Johnson <Jared@Jareds-MBP.home>
2016-10-12 01:07:48 -0400

    work on resizing element

--- frontend/components/knowtation/notation_view.jsx
+++ frontend/components/knowtation/notation_view.jsx
@@ -2,19 +2,14 @@ import React from 'react';
 import Canvas from 'react-canvas-component';
 
 class NotationView extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
 
   componentWillReceiveProps(newProps) {
     const { knowtation } = newProps;
-    const image = new Image();
-    image.src = knowtation.notationImageUrl;
-    console.log(knowtation.notationImageUrl);
-    const canvas = document.getElementById('canvas');
-    const context = canvas.getContext('2d');
-    image.onload = () => {
-      context.drawImage(image,
-        0, 0, image.width, image.height
-      );
-    };
+    this.draw(knowtation);
   }
 
   render() {
@@ -23,6 +18,17 @@ class NotationView extends React.Component {
         <canvas id='canvas' className='notation-view' />
       </div>
     );
+  }
+
+  // helpers
+
+  draw(knowtation) {
+    const ctx = document.getElementById('canvas').getContext('2d');
+    const img = new Image ();
+    img.onload = () => {
+      ctx.drawImage(img, 0, 0, img.width, img.width, 0, 0, 500, 500);
+    };
+    img.src = knowtation.notationImageUrl;
   }
 }
 
