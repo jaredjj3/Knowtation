c4f83b41928d0f03addce9f80cac2c6d80b49e79
Jared Johnson <Jared@Jareds-MacBook-Pro.local>
2016-10-13 19:30:31 -0400

    get frontend tags portion to work, start on backend

--- frontend/components/knowtation/library.jsx
+++ frontend/components/knowtation/library.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import { Link } from 'react-router';
 
 class Library extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.onMouseEnterHandler = this.onMouseEnterHandler.bind(this);
+    this.onMouseLeaveHandler = this.onMouseLeaveHandler.bind(this);
+  }
+
   componentDidMount() {
     if (this.props.knowtations.length < 24) {
       this.props.requestAllKnowtations();
     }
   }
-
-  className
 
   render() {
     const { knowtations } = this.props;
@@ -17,14 +22,20 @@ class Library extends React.Component {
       const author = knowtation.user;
       return (
         <li className='library-list-item' key={ idx }>
-          <div className='library-list-knowtation-link'>
+          <div
+            id={ knowtation.id }
+            className='library-list-knowtation-link'
+            onMouseEnter={ this.onMouseEnterHandler }
+            onMouseLeave={ this.onMouseLeaveHandler }
+          >
             <Link to={ `/knowtation/${knowtation.id}` }>
-              <img src={ knowtation.thumbnailUrl }/>
+              <img id={ knowtation.id } src={ knowtation.thumbnailUrl } />
               <div
-                className='hide-metadata-knowtation'
-                onHover={ this.handleHover }
+                id={ `library-metadata-${ knowtation.id }` }
+                className='hide-metadata'
               >
-
+                { knowtation.receivedLoops }
+                <i className="material-icons">loop</i>
               </div>
               <h2>{ knowtation.title }</h2>
             </Link>
@@ -43,6 +54,20 @@ class Library extends React.Component {
           </ul>
       </div>
     );
+  }
+
+  // event handlers
+
+  onMouseEnterHandler(e) {
+    const id = e.currentTarget.id;
+    const idName = `library-metadata-${ id }`;
+    document.getElementById(idName).className = 'show-metadata';
+  }
+
+  onMouseLeaveHandler(e) {
+    const id = e.currentTarget.id;
+    const idName = `library-metadata-${ id }`;
+    document.getElementById(idName).className = 'hide-metadata';
   }
 }
 
